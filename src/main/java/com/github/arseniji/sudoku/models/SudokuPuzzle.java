faeb102cd4e64eff1b1ea27089b75c2087147f81
arseniji <odiousguyfromnet@gmail.com>
2026-03-15 16:05:29 +0300

    обёртка

---
+++ src/main/java/com/github/arseniji/sudoku/models/SudokuPuzzle.java
@@ -0,0 +1,24 @@
+package com.github.arseniji.sudoku.models;
+
+import com.github.arseniji.sudoku.enums.Difficulty;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public class SudokuPuzzle {
+    private final UUID id;
+    private final Cell[][] cells;
+    private final Difficulty difficulty;
+    private final int[][] solution;
+    private final LocalDateTime createdAt;
+
+    public SudokuPuzzle(Cell[][] cells, int[][] solution, Difficulty difficulty){
+        this.id = UUID.randomUUID();
+        this.cells = cells;
+        this.solution = solution;
+        this.difficulty = difficulty;
+        this.createdAt = LocalDateTime.now();
+    }
+
+}
