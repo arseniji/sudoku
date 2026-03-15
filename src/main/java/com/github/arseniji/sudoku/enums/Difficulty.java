68bb36f4b370f9c9b5ff94586c8a7e4c3c4f4e52
arseniji <odiousguyfromnet@gmail.com>
2026-03-15 17:54:56 +0300

    format changed

--- src/main/java/com/github/arseniji/sudoku/enums/Difficulty.java
+++ src/main/java/com/github/arseniji/sudoku/enums/Difficulty.java
@@ -1,9 +1,9 @@
 package com.github.arseniji.sudoku.enums;
 
 public enum Difficulty {
-    Easy(60),
-    Medium(40),
-    Hard(20);
+    EASY(60),
+    MEDIUM(40),
+    HARD(20);
     private final int value;
     Difficulty(int value){
         this.value = value;
